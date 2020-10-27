b414aab05a3e0876ad6a73bc44d24310c824191b
‘hama <https://github.com/th-ht-80d/MyOclock.git>
2020-10-27 23:04:47 +0900

    Initial Commit

---
+++ MyOclock/StopWatch.swift
@@ -0,0 +1,77 @@
+//
+//  StopWatch.swift
+//  MyOclock
+//
+//  Created by 濱貴大 on 2020/10/01.
+//  Copyright © 2020 thama. All rights reserved.
+//
+
+import UIKit
+
+class StopWatch: UIViewController {
+
+    var countNum = 0
+    var timerRunning = false
+    var timer = Timer()
+    
+    @objc func updateDisplay(){
+           countNum += 1
+           let ms = countNum % 100
+           let s = (countNum - ms) / 100 % 60
+           let m = (countNum - s - ms) / 6000 % 3600
+           timeDisplay.text = String(format: "%02d:%02d.%02d", m,s,ms)
+       }
+    
+    
+    @IBOutlet weak var timeDisplay: UILabel!
+    
+    @IBAction func StartButton(_ sender: Any) {
+        
+        if timerRunning == false{
+            timer = Timer.scheduledTimer(timeInterval: 0.01, target: self, selector: #selector(StopWatch.updateDisplay), userInfo: nil, repeats: true)
+            
+            timerRunning = true
+        }
+        
+    }
+    
+    
+    @IBAction func StopButton(_ sender: Any) {
+        
+        if timerRunning == true{
+                    timer.invalidate()
+                    timerRunning = false
+                }
+        
+    }
+    
+    
+    @IBAction func ClearButton(_ sender: Any) {
+        countNum = 0
+        timeDisplay.text = "00:00.00"
+        
+        
+    }
+    
+    
+    override func viewDidLoad() {
+        
+        
+        super.viewDidLoad()
+        
+        
+        
+        
+        
+
+        
+    }
+    
+    @IBAction func Back(_ sender: Any) {
+        //戻る
+    dismiss(animated: true, completion: nil)
+        
+    }
+    
+    
+}
---
+++ MyOclock/ViewController.swift
@@ -0,0 +1,121 @@
+//
+//  ViewController.swift
+//  MyOclock
+//
+//  Created by 濱貴大 on 2020/09/27.
+//  Copyright © 2020 thama. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+//時間
+    @IBOutlet weak var NowTime: UILabel!
+    
+    let dt = Date()
+    let dateFormatter = DateFormatter()
+    
+    
+   //ストップウォッチのボタン
+    @IBAction func StopWatch(_ sender: Any) {
+        
+        
+        
+        
+    }
+    
+   //日付
+    @IBOutlet weak var NowDay: UILabel!
+    
+    
+        
+    
+    override func viewDidLoad() {
+      
+        super.viewDidLoad()
+        
+        
+        //１秒毎に進む
+        Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(ViewController.timerUpdate), userInfo: nil, repeats: true)
+        
+    
+           
+        //日付設定
+        dateFormatter.dateFormat = DateFormatter.dateFormat(fromTemplate: "yMMMd(EEE)", options: 0,locale: Locale(identifier: "ja_JP"))
+        
+        let date = dateFormatter.string(from: dt)
+        print(date)
+        
+        //↑変数　日付表示
+        NowDay.text = date
+        
+        let arrWeekday: Array = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
+        
+       let date1 = NSDate()
+        let calendar = NSCalendar(calendarIdentifier: NSCalendar.Identifier.gregorian)
+
+        
+        _ = calendar?.component(.year, from: date1 as Date)
+        
+        _ = calendar?.component(.month, from: date1 as Date)
+        
+        _ = calendar?.component(.day, from: date1 as Date)
+        
+        let weekday = calendar?.component(.weekday, from: date1 as Date)
+        
+    
+        _ = arrWeekday[weekday! - 1]
+        
+
+        //時間設定
+       let date2:Date = Date()
+        let format = DateFormatter()
+        format.dateFormat = "HH:mm:ss"
+        
+        let Date2 = format.string(from: date2)
+        
+        print(Date2)
+        
+        NowTime.text =  Date2
+        
+        //時
+        _ = calendar?.component(.hour, from: date1 as Date)
+        
+        //分
+        _ = calendar?.component(.minute, from: date1 as Date)
+        
+        
+        //秒
+        _ = calendar?.component(.second, from: date1 as Date)
+        
+        
+        
+        
+        
+        
+        
+    }
+    //１秒毎に進む内容を記載
+    @objc func timerUpdate() {
+        
+        
+        let date2:Date = Date()
+        let format = DateFormatter()
+        format.dateFormat = "HH:mm:ss"
+        
+        let Date2 = format.string(from: date2)
+        print("update")
+        
+        NowTime.text =  Date2
+        
+    }
+    
+    
+    
+    
+    
+    
+
+}
+
