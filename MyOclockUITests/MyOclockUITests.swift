45eaede60bb350186d4623780d97a747cc2ff1a7
hama <21331104th@tama.ac.jp>
2021-01-25 23:32:08 +0900

    サンプルテストコード 記載

--- MyOclockUITests/MyOclockUITests.swift
+++ MyOclockUITests/MyOclockUITests.swift
@@ -31,6 +31,10 @@ class MyOclockUITests: XCTestCase {
         // Use recording to get started writing UI tests.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
     }
+    
+    func testDBManagerTest_findAllMethod_whenNotExistData_output0Data() {
+                // テスト
+              }
 
     func testLaunchPerformance() throws {
         if #available(macOS 10.15, iOS 13.0, tvOS 13.0, *) {
@@ -38,6 +42,8 @@ class MyOclockUITests: XCTestCase {
             measure(metrics: [XCTApplicationLaunchMetric()]) {
                 XCUIApplication().launch()
             }
+            
+
         }
     }
 }
